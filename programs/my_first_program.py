3b50a07ad8b4c40f6ecb551ea96144fbd0c519df
athul-22 <dev.athulnambiar@gmail.com>
2024-06-26 05:03:58 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,14 +1,12 @@
 from nada_dsl import *
 
-
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # Perform a computation
-    result = my_int1 + my_int2  # Example computation: adding the two secret integers
+    # Perform a different computation
+    result = my_int1 - my_int2  # Example computation: subtracting the second secret integer from the first
 
     # Output the result
     return [Output(result, "my_output", party1)]
-
